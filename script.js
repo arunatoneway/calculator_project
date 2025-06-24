34bc82e30adbc7c937f9d461e905a1a70d624451
Arun Thomas <arunatoneway@gmail.com>
2025-06-25 00:02:09 +0530

    enabled the equal button to identify first and last chara entered and clears if they are symbols and also added addition fuction to equal press event but the value is returning NaN, need to resolve that and add other operations

--- script.js
+++ script.js
@@ -18,76 +18,151 @@ const squered = document.getElementById('squered')
 const cubed = document.getElementById('cubed')
 const equal = document.getElementById('equal')
 const clear = document.getElementById('clear')
-let wincont =""
+let wincont = ""
 
 console.log(num1)
 
-function adno(winText,btnval){
+function adno(winText, btnval) {
     winText += btnval;
     console.log(winText)
     return winText;
 }
-function clrno(winText){
-    winText = winText.slice(0,winText.length - 1);
+function clrno(winText) {
+    winText = winText.slice(0, winText.length - 1);
     return winText;
 
 }
 
-num1.addEventListener('click',() => {wincont = adno(wincont,"1");
+function calc(winText) {
+    let j = ""
+
+    let ad2 = ""
+
+    let final = ""
+
+
+
+
+    for (let i = 0; i < winText.length; i++) {
+
+
+        let a = winText.length
+        if (["+", "-", "*", "/", "^"].includes(winText[0]) || ["+", "-", "*", "/", "^"].includes(winText[a - 1])) {
+            winText = ""
+            return winText;
+        }
+
+        let y = winText[i];
+        if (y == "+") {
+
+            for (let r = i + 1; r <= winText.length + 1; r++) {
+
+
+
+                let d = winText[r];
+                if (["+", "-", "*", "/", "^"].includes(winText[r]) || r == winText.length + 1) {
+
+                    j = (1 * j) + (1 * ad2);
+                    final = j;
+                    break;
+
+                }
+                ad2 += d;
+
+
+            }
+
+
+
+        }
+
+
+
+        j += y;
+
+
+    }
+    return winText = final;
+
+
+
+
+
+}
+
+num1.addEventListener('click', () => {
+    wincont = adno(wincont, "1");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num2.addEventListener('click',() => {wincont = adno(wincont,"2");
+num2.addEventListener('click', () => {
+    wincont = adno(wincont, "2");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
 
 
-num3.addEventListener('click',() => {wincont = adno(wincont,"3");
+num3.addEventListener('click', () => {
+    wincont = adno(wincont, "3");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
 
-num4.addEventListener('click',() => {wincont = adno(wincont,"4");
+num4.addEventListener('click', () => {
+    wincont = adno(wincont, "4");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num5.addEventListener('click',() => {wincont = adno(wincont,"5");
+num5.addEventListener('click', () => {
+    wincont = adno(wincont, "5");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num6.addEventListener('click',() => {wincont = adno(wincont,"6");
+num6.addEventListener('click', () => {
+    wincont = adno(wincont, "6");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num7.addEventListener('click',() => {wincont = adno(wincont,"7");
+num7.addEventListener('click', () => {
+    wincont = adno(wincont, "7");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num8.addEventListener('click',() => {wincont = adno(wincont,"8");
+num8.addEventListener('click', () => {
+    wincont = adno(wincont, "8");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num9.addEventListener('click',() => {wincont = adno(wincont,"9");
+num9.addEventListener('click', () => {
+    wincont = adno(wincont, "9");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-num0.addEventListener('click',() => {wincont = adno(wincont,"0");
+num0.addEventListener('click', () => {
+    wincont = adno(wincont, "0");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-plus.addEventListener('click',() => {wincont = adno(wincont,"+");
+plus.addEventListener('click', () => {
+    wincont = adno(wincont, "+");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
 
-minus.addEventListener('click',() => {wincont = adno(wincont,"-");
+minus.addEventListener('click', () => {
+    wincont = adno(wincont, "-");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-mult.addEventListener('click',() => {wincont = adno(wincont,"*");
+mult.addEventListener('click', () => {
+    wincont = adno(wincont, "*");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-divise.addEventListener('click',() => {wincont = adno(wincont,"/");
+divise.addEventListener('click', () => {
+    wincont = adno(wincont, "/");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-squered.addEventListener('click',() => {wincont = adno(wincont,"^2");
+squered.addEventListener('click', () => {
+    wincont = adno(wincont, "^2");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-cubed.addEventListener('click',() => {wincont = adno(wincont,"^3");
+cubed.addEventListener('click', () => {
+    wincont = adno(wincont, "^3");
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-equal.addEventListener('click',() => {wincont = adno(wincont,"^3");
+equal.addEventListener('click', () => {
+    wincont = calc(wincont);
     document.getElementById('inpWindow').innerHTML = wincont;
 })
-clear.addEventListener('click',() => {wincont = clrno(wincont);
+clear.addEventListener('click', () => {
+    wincont = clrno(wincont);
     document.getElementById('inpWindow').innerHTML = wincont;
 })
